6ed93f809d9ce842d90bd84afe876a144936d16a
kohlkuntze <kohlkuntze@gmail.com>
2018-09-02 23:38:48 -0400

    Made updates

--- TrafficTools/src/com/model/Road.java
+++ TrafficTools/src/com/model/Road.java
@@ -1,11 +1,13 @@
 package com.model;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.util.List;
 
 @Builder
+@Getter
 public class Road {
 
     @NonNull
@@ -14,4 +16,10 @@ public class Road {
     @NonNull
     private final List<Automobile> outQueue;
 
+    @NonNull
+    private Double speedLimit;
+
+    private void setSpeedLimit(double newSpeedLimit) {
+        speedLimit = newSpeedLimit;
+    }
 }
